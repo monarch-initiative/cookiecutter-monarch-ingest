21ca812b999ce747bec7f638adadb54a12ac76bd
glass-ships <glass.ships@outlook.com>
2024-04-19 16:31:58 -0600

    some more edits

---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/transform.py
@@ -0,0 +1,11 @@
+import uuid
+
+from koza.cli_runner import get_koza_app
+
+from biolink_model.datamodel.pydanticmodel_v2 import * # Replace * with any necessary data classes from the Biolink Model
+
+koza_app = get_koza_app("{{cookiecutter.ingest_name}}")
+
+while (row := koza_app.get_row()) is not None:
+    # Code to transform each row of data
+    ...
