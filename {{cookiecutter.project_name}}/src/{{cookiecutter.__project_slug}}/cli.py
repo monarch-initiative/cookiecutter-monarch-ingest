95fdc2daabdfbd71be2734b92ad3fc350fad4c8c
glass-ships <glass.ships@outlook.com>
2024-04-22 17:14:57 -0600

    add functional(?) default transform

--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/cli.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/cli.py
@@ -1,22 +1,30 @@
 """Command line interface for {{cookiecutter.project_name}}."""
 import logging
 
-import click
 from koza.cli_runner import transform_source
+import typer
 
+app = typer.Typer()
 logger = logging.getLogger(__name__)
 
 
-@click.command()
-@click.option("-o", "--output-dir", default="output", help="Output directory for transformed data")
-@click.option("-r", "--row-limit", default=None, help="Number of rows to process")
-@click.option("-v", "--verbose", default=False, help="Whether to be verbose")
-def main(
-    output_dir: str,
-    row_limit: int,
-    verbose: int
+@app.callback()
+def callback(version: bool = typer.Option(False, "--version", is_eager=True),
 ):
-    """Run the Koza transform for {{cookiecutter.project_name}}"""
+    """{{cookiecutter.project_name}} CLI."""
+    if version:
+        from {{cookiecutter.__project_slug}} import __version__
+
+        typer.echo(f"{{cookiecutter.project_name}} version: {__version__}")
+        raise typer.Exit()
+
+@app.command()
+def main(
+    output_dir: str = typer.Option("output", help="Output directory for transformed data"),
+    row_limit: int = typer.Option(None, help="Number of rows to process"),
+    verbose: int = typer.Option(False, help="Whether to be verbose"),
+):
+    """Run the Koza transform for {{cookiecutter.project_name}}."""
     transform_source(
         source='transform.py',
         output_dir=output_dir,
--- {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/transform.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/transform.py
@@ -1,12 +1,17 @@
-import uuid
-
-from koza.cli_runner import get_koza_app
+import uuid # For generating UUIDs for associations
 
 from biolink_model.datamodel.pydanticmodel_v2 import * # Replace * with any necessary data classes from the Biolink Model
+from koza.cli_runner import get_koza_app
 
 koza_app = get_koza_app("{{cookiecutter.__ingest_name}}")
 
 while (row := koza_app.get_row()) is not None:
     # Code to transform each row of data
     # For more information, see https://koza.monarchinitiative.org/Ingests/transform
-    ...
+    entity = {
+        "id": str(uuid.uuid1()),
+        "subject": row["example_column_1"],
+        "predicate": row["example_column_3"],
+        "object": row["example_column_2"],
+    }
+    koza_app.write(entity)
