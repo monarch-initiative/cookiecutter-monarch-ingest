711cf1798898a9c7e8dfd4196a42acb7b67ddde0
glass-ships <glass.ships@outlook.com>
2024-04-22 17:31:18 -0600

    use slug instead of name in some places

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,23 +3,21 @@
 import re
 import sys
 
-MODULE_REGEX = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
+MODULE_REGEX = re.compile(r"^[_a-zA-Z][_a-zA-Z0-9]+$")
 
-project_name = '{{ cookiecutter.project_name}}'
+module_name = "{{ cookiecutter.__project_slug}}"
 
-if not MODULE_REGEX.match(project_name):
-    if "-" in project_name:
+if not MODULE_REGEX.match(module_name):
+    if "-" in module_name:
         invalid_char = "-"
-    elif " " in project_name:
+    elif " " in module_name:
         invalid_char = "<space>"
     else:
         print(
-            f'ERROR: {project_name} is not a valid Python module name. Try again with a valid project name.'
+            f"ERROR: {module_name} is not a valid Python module name. Try again with a valid project name."
         )
         # Exit to cancel project
         sys.exit(1)
     print(
-        f'WARNING: {project_name} is not a valid Python module name. Using "_" instead of {invalid_char} for module name.'
+        f'WARNING: {module_name} is not a valid Python module name. Using "_" instead of "{invalid_char}" for module name.'
     )
-
-    
