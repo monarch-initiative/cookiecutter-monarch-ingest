9d460842a890ae3511b1f7c5ccc4b3c26b8003f3
glass-ships <glass.ships@outlook.com>
2024-05-01 13:07:38 -0600

    fix test after changes

--- {{cookiecutter.project_name}}/tests/test_example.py
+++ {{cookiecutter.project_name}}/tests/test_example.py
@@ -1,20 +1,21 @@
 """
 An example test file for the transform script.
 
-It uses pytest fixtures to define the input data and the mock koza transform. 
+It uses pytest fixtures to define the input data and the mock koza transform.
 The test_example function then tests the output of the transform script.
 
 See the Koza documentation for more information on testing transforms:
 https://koza.monarchinitiative.org/Usage/testing/
 """
 
-import pytest 
+import pytest
 
 from koza.utils.testing_utils import mock_koza
 
 # Define the ingest name and transform script path
 INGEST_NAME = "{{cookiecutter.__project_slug}}"
 TRANSFORM_SCRIPT = "./src/{{cookiecutter.__project_slug}}/transform.py"
+
 
 # Define an example row to test (as a dictionary)
 @pytest.fixture
@@ -24,6 +25,7 @@ def example_row():
         "example_column_2": "entity_6",
         "example_column_3": "biolink:related_to",
     }
+
 
 # Or a list of rows
 @pytest.fixture
@@ -41,36 +43,43 @@ def example_list_of_rows():
         },
     ]
 
+
 # Define the mock koza transform
 @pytest.fixture
 def mock_transform(mock_koza, example_row):
+    # Returns [entity_a, entity_b, association] for a single row
     return mock_koza(
         INGEST_NAME,
         example_row,
         TRANSFORM_SCRIPT,
     )
 
+
 # Or for multiple rows
 @pytest.fixture
 def mock_transform_multiple_rows(mock_koza, example_list_of_rows):
+    # Returns concatenated list of [entity_a, entity_b, association]
+    # for each row in example_list_of_rows
     return mock_koza(
         INGEST_NAME,
         example_list_of_rows,
         TRANSFORM_SCRIPT,
     )
 
+
 # Test the output of the transform
 
+
 def test_single_row(mock_transform):
-    assert len(mock_transform) == 1
+    assert len(mock_transform) == 3
     entity = mock_transform[0]
     assert entity
-    assert entity.subject == "entity_1"
+    assert entity.name == "entity_1"
 
 
 def test_multiple_rows(mock_transform_multiple_rows):
-    assert len(mock_transform_multiple_rows) == 2
-    entity_1 = mock_transform_multiple_rows[0]
-    entity_2 = mock_transform_multiple_rows[1]
-    assert entity_1.subject == "entity_1"
-    assert entity_2.subject == "entity_2"
+    assert len(mock_transform_multiple_rows) == 6
+    entity_a = mock_transform_multiple_rows[0]
+    entity_b = mock_transform_multiple_rows[1]
+    assert entity_a.name == "entity_1"
+    assert entity_b.name == "entity_6"
