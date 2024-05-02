00072d0e702d25eb7ad8a50d50a8542a0a1b0be9
glass-ships <glass.ships@outlook.com>
2024-05-02 12:48:13 -0600

    add reponame to cookiecutter

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,10 +3,22 @@
 import re
 import sys
 
+REPO_NAME_REGEX = re.compile(r"^[a-zA-Z0-9_-]+$")
 MODULE_REGEX = re.compile(r"^[_a-zA-Z][_a-zA-Z0-9]+$")
 
-module_name = "{{ cookiecutter.__project_slug}}"
+repo_name = "{{ cookiecutter.__repo_name }}"
+module_name = "{{ cookiecutter.__project_slug }}"
 
+# Assert repo name is dash-separated
+if not REPO_NAME_REGEX.match(repo_name):
+    print(
+        f"""ERROR: {repo_name} is not a valid GitHub repository name.
+Try again with a valid project name using only '-', '_', '.', or spaces as separators.
+"""
+    )
+    sys.exit(1)
+
+# Check if the module name is a valid Python module name
 if not MODULE_REGEX.match(module_name):
     if "-" in module_name:
         invalid_char = "-"
@@ -16,8 +28,7 @@ if not MODULE_REGEX.match(module_name):
         print(
             f"ERROR: {module_name} is not a valid Python module name. Try again with a valid project name."
         )
-        # Exit to cancel project
         sys.exit(1)
     print(
-        f'WARNING: {module_name} is not a valid Python module name. Using "_" instead of "{invalid_char}" for module name.'
+        f'WARNING: {module_name} is not a valid Python module name. Try using "_" instead of "{invalid_char}" for module name.'
     )
--- {{cookiecutter.project_name}}/scripts/get-latest-report.py
+++ {{cookiecutter.project_name}}/scripts/get-latest-report.py
@@ -2,24 +2,36 @@ import requests
 import json
 
 
-def get_latest_reports():
-    url = f"https://api.github.com/repos/{{ cookiecutter.github_org_name }}/{{ cookiecutter.__project_slug }}/releases/latest"
+def main():
+    url = (
+        "https://api.github.com/repos/{{ cookiecutter.github_org_name }}/{{ cookiecutter.__repo_name }}/releases/latest"
+    )
+
+    # Get the latest release from the GitHub API
     response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(
+            f"\n\tFailed to get latest release from {url}\n\tStatus Code: {response.status_code} - {response.text}"
+        )
     data = json.loads(response.text)
 
-    reports = []
+    # Get the download URLs for the reports
+    reports = {}
     for asset in data["assets"]:
+        report_name = asset["name"]
         if "report" in asset["name"].split("_"):
             file_url = asset["browser_download_url"]
-            reports.append(file_url)
+            reports[report_name] = file_url
 
-    return reports
+    if not reports:
+        raise Exception("No reports found in the latest release")
 
-
-def download_reports(reports):
-    for report in reports:
-        response = requests.get(report)
-        with open(f"docs/{report.split('/')[-1]}", "wb") as f:
+    # Download the reports
+    for fn, url in reports:
+        response = requests.get(url)
+        output_fn = "_".join(fn.split("_")[:-1])
+        with open(f"docs/{output_fn}", "wb") as f:
             f.write(response.content)
 
-print(get_latest_reports())
+
+print(main())
